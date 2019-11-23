25348db302c0a59e119b3449e07938fc5cdd83e7
Phoomparin Mano <phoomparin@gmail.com>
2019-11-23 11:05:08 +0700

    :sparkles: Make table scrollable in mobile

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,7 +1,18 @@
 import 'whatwg-fetch'
 
+function wrapTable() {
+  const tables = [...document.querySelectorAll('table')]
+
+  tables.forEach(table => {
+    const wrapper = document.createElement('div')
+    wrapper.classList.add('table-container')
+    table.parentNode.insertBefore(wrapper, table)
+    wrapper.appendChild(table)
+  })
+}
+
 async function onReady() {
-  console.log('Page Loaded.')
+  wrapTable()
 }
 
 document.addEventListener('DOMContentLoaded', onReady)
